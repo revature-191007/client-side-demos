c015b5961b0f135aa65910a17ae69a679a5b9a2e
Mitch Goshorn <msgoshorn@gmail.com>
2019-10-23 11:55:07 -0400

    Added basic js intro

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+// What is JavaScript?
+
+/** JavaScript is a programming language.
+ *  JavaScript is the programming language that web browsers understand.
+ *  
+ *  Java is statically and strongly typed.
+ *  JavaScript is dynamically typed, features implicit type coersion, asynchronous,
+ *      language supports OOP and functional programming styles.
+ * 
+ *  JavaScript started out primarily as a browser language. 
+ * 
+ * We will focus on ES6 JavaScript.
+ * ES - EcmaScript - Standardizations of JavaScript
+ * ECMA - European Computer Manufacturing Association - Which is the body that maintains
+ * standards for the JS language.
+ * 
+*/
---
+++ js/types.js
@@ -0,0 +1,16 @@
+// Using Code Runner to run code locally
+
+// declaring variable x with value of 10
+let x = 10; // number - (Same as Java double)
+x = 'hello'; // string
+x = true; // boolean
+x = []; // object
+x = {}; // <--- object-literal type: object
+x = null; // object or null
+x = undefined; // undefined
+x = NaN; // number
+x = Symbol('hello'); // symbol
+x = () => {}; // function
+
+// print to console
+console.log(typeof x);
